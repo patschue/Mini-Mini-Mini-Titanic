a7f108d1f11b0bd89bbcf72cee08d228867d0865
patschue <84413011+patschue@users.noreply.github.com>
2021-09-16 13:59:35 +0200

    Add files via upload

---
+++ TitanicTree.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 13:30:36 2021
+
+@author: Patrick
+"""
+import seaborn as sns
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+# Laden der Daten
+Titanic = pd.read_csv("minichallenge_titanic.csv")
+
+# Transformation der Variable Geschlecht
+labelenc = preprocessing.LabelEncoder()
+labelenc.fit(Titanic.Sex)
+Titanic['sex_transform'] = labelenc.transform(Titanic.Sex)
+
+# Auswahl von Daten zum Training
+train = Titanic.loc[:,['Age', 'Fare', 'sex_transform', 'Survived']].dropna()
+
+# Aufteilen der Daten in Features und Target
+y = train['Survived']
+X = train.drop('Survived', axis=1)
+
+# Aufteilen in Test und Train Daten
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=10, shuffle = True)
+
+# Initialisierung des Entscheidungsbaums
+clf = DecisionTreeClassifier(max_depth=2)
+
+# Training des Entscheidungsbaums
+clf.fit(X_train, y_train)
+
+# Berechnung der Metrik
+clf.score(X_test, y_test)
+
+tree.plot_tree(clf)
+plt.figure(figsize=(40,20))
+tree.plot_tree(clf, fontsize=23)
+plt.show()
